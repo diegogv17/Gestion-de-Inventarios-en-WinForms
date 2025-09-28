8fe64dab57aa5dee9199e9f55ea2b3900c883752
Diego Gómez <DG6488379@gmail.com>
2025-09-27 20:05:01 -0600

    Agregar archivos de proyecto.

---
+++ Gestión de Inventarios en WinForms/Form1.cs
@@ -0,0 +1,10 @@
+namespace Gestión_de_Inventarios_en_WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
